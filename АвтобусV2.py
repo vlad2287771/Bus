c10b0c8dc43b5aeddcfc0cfc8cd8d61235d31be0
vlad2287771 <101133573+vlad2287771@users.noreply.github.com>
2022-03-09 10:17:09 +0500

    Update АвтобусV2.py

--- АвтобусV2.py
+++ АвтобусV2.py
@@ -1,207 +1,27 @@
-print(' Номер маршрута \n(маршруты с буквами - тот же транспорт)\n вместо 0 вначале писать цифру 4')
-a = int(input())
-
-
-if a==24:
-    print("автобус")
-elif a==25:
-    print("автобус")
-elif a==27:
-    print("автобус")
-elif a==28:
-    print("автобус")
-elif a==36:
-    print("автобус")
-elif a==37:
-    print("автобус")
-elif a==43:
-    print("автобус")
-elif a==50:
-    print("автобус")
-elif a==54:
-    print("автобус")
-elif a==57:
-    print("автобус")
-elif a==58:
-    print("автобус")
-elif a==59:
-    print("автобус")
-elif a==60:
-    print("автобус")
-elif a==61:
-    print("автобус")
-elif a==65:
-    print("автобус")
-elif a==69:
-    print("автобус")
-elif a==71:
-    print("автобус")
-elif a==75:
-    print("автобус")
-elif a==76:
-    print("автобус")
-elif a==85:
-    print("автобус")
-elif a==88:
-    print("автобус")
-elif a==90:
-    print("автобус")
-elif a==91:
-    print("автобус")
-elif a==95:
-    print("автобус")
-elif a==96:
-    print("автобус")
-elif a==97:
-    print("автобус")
-elif a==99:
-    print("автобус")
-    
-elif a==1:
-    print("троллейбус или трамвай")
-elif a==2:
-    print("трамвай")
-elif a==3:
-    print("троллейбус или трамвай")
-elif a==4:
-    print("троллейбус или трамвай")
-elif a==5:
-    print("троллейбус или трамвай")
-elif a==6:
-    print("троллейбус или трамвай")
-elif a==7:
-    print("троллейбус или трамвай")
-elif a==8:
-    print("троллейбус или трамвай")
-elif a==9:
-    print("троллейбус или трамвай")
-elif a==10:
-    print("троллейбус или трамвай")
-elif a==11:
-    print("троллейбус или трамвай")
-elif a==12:
-    print("троллейбус или маршрутка или трамвай")
-elif a==13:
-    print("троллейбус или трамвай")
-elif a==14:
-    print("троллейбус или трамвай")
-elif a==15:
-    print("троллейбус или трамвай")
-elif a==16:
-    print("троллейбус или трамвай")
-elif a==17:
-    print("троллейбус или трамвай")
-elif a==18:
-    print("троллейбус или трамвай")
-elif a==19:
-    print("троллейбу или трамвайс")
-elif a==20:
-    print("троллейбус или трамвай")
-    
-elif a==21:
-    print("трамвай")
-elif a==22:
-    print("трамвай")
-elif a==23:
-    print("трамвай")
-elif a==24:
-    print("трамвай")
-elif a==25:
-    print("трамвай")
-elif a==26:
-    print("трамвай")
-elif a==27:
-    print("трамвай")
-elif a==32:
-    print("трамвай")
-elif a==33:
-    print("трамвай")
-elif a==34:
-    print("трамвай")
-    
-elif a==53:
-    print("маршрутка")
-elif a==68:
-    print("маршрутка")
-elif a==80:
-    print("маршрутка")
-elif a==81:
-    print("маршрутка")
-elif a==49:
-    print("маршрутка")
-    
-elif a==44:
-    print("маршрутка")
-elif a==48:
-    print("маршрутка")
-elif a==92:
-    print("маршрутка")
-elif a==447:
-    print("маршрутка")
-elif a==453:
-    print("маршрутка")
-    
-elif a==45:
-    print("маршрутка")
-elif a==412:
-    print("маршрутка")
-elif a==419:
-    print("маршрутка")
-elif a==26:
-    print("маршрутка")
-elif a==46:
-    print("маршрутка")
-    
-elif a==46:
-    print("маршрутка")
-elif a==414:
-    print("маршрутка")
-elif a==56:
-    print("маршрутка")
-elif a==73:
-    print("маршрутка")
-elif a==482:
-    print("маршрутка")
-elif a==483:
-    print("маршрутка")
-    
-elif a==40:
-    print("маршрутка")
-elif a==38:
-    print("маршрутка")
-elif a==442:
-    print("маршрутка")
-elif a==470:
-    print("маршрутка")
-    
-elif a==416:
-    print("маршрутка")
-elif a==430:
-    print("маршрутка")
-elif a==42:
-    print("маршрутка")
-elif a==452:
-    print("маршрутка")
-elif a==94:
-    print("маршрутка")
-elif a==477:
-    print("маршрутка")
-    
-elif a==45:
-    print("маршрутка")
-elif a==63:
-    print("маршрутка")
-elif a==456:
-    print("маршрутка")
-elif a==454:
-    print("маршрутка")
-elif a==79:
-    print("маршрутка")
-    
-else:
-    print('нет маршрута')
-
-
+print('Введите номер маршрута.')
+a = input()
+b = 0
+bus = '24, 25, 27, 28, 36, 37, 43, 50, 54, 57, 57а, 58, 59, 60, 61, 65, 69, 71, 75, 76, 85, 88, 90, 91, 91м, 95, 96, 97, 99'.split(', ')
+tram = '1, 2, 3, 4, 5, 5а, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 32, 33, 34'.split(', ')
+trolleybus = '1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 33'.split(', ')
+minibus = '53, 68, 80, 81, 09, 44, 48, 92, 047, 053, 05, 012, 019, 26, 46, 06, 014, 56, 73, 082, 083, 40, 12, 38, 042, 070, 016, 030, 42, 052, 94, 077, 45, 63, 056, 054, 79'.split(', ')
+for number in bus:
+    if number == a:
+        b += 1
+        print("Автобус", end= '. ')
+for number in tram:
+    if number == a:
+        b += 1
+        print("Трамвай", end= '. ')
+for number in trolleybus:
+    if number == a:
+        b += 1
+        print("Троллейбус", end= '. ')
+for number in minibus:
+    if number == a:
+        print("Маршрутка", end= '. ')
+if b == 0:
+    print("Нет маршрута.")
 
 
 
